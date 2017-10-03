742b2acf44004f597ba35fd49d475f434e5e3506
Brandon  Harris <cheeridon@gmail.com>
2017-10-03 22:17:07 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -37,5 +37,5 @@ def selective_tweet_shortener tweet
 end
 
 def shortened_tweet_truncator tweet
-  selective_tweet_shortener(tweet).length > 140 ? word_substituter(tweet).concat("...") : tweet 
+  selective_tweet_shortener(tweet).length > 140 ? word_substituter(tweet)[0...140].concat("...") : tweet 
 end
